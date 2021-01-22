d88d90aa6386635e07ef84e7517da6b47394c1f4
matheus f <math.fmartins42@gmail.com>
2021-01-22 08:53:25 -0300

    lazy loading at home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [
